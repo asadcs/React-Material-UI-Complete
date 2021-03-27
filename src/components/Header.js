109af94b640f4f328fc8afa80c9da5d9e5d72857
Muhammad Asadullah <muhammad.asad2020@gmail.com>
2021-03-27 01:44:05 -0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,10 +8,12 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import { Badge, Grid, InputBase } from '@material-ui/core';
 import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+import SearchIcon from '@material-ui/icons/Search';
 
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
+        backgroundColor:'#fff'
     },
     menuButton: {
         marginRight: theme.spacing(2),
@@ -19,6 +21,17 @@ const useStyles = makeStyles((theme) => ({
     title: {
         flexGrow: 1,
     },
+    searchInput: {
+        opacity: '0.6',
+        padding : '0px 8px',
+        fontSize : '0.8rem',
+        '&:hover':{
+            backgroundColor:'#f2f2f2'
+        },
+        '& .MuiSvgIcon-root':{
+            marginRight:'8px'
+        }
+    }
 }));
 
 
@@ -26,11 +39,11 @@ export default function Header() {
     const classes = useStyles();
 
     return (
-        <AppBar position="static">
+        <AppBar position="static" className={classes.root}>
             <Toolbar>
-                <Grid container>
+                <Grid container alignItems="center">
                     <Grid item  >
-                        <InputBase />
+                        <InputBase placeholder="Search" className={classes.searchInput} startAdornment={<SearchIcon fontSize="small"/>}/>
                     </Grid>
                     <Grid item sm >
                         <InputBase />
@@ -38,17 +51,17 @@ export default function Header() {
                     <Grid item  >
                         <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
                             <Badge badgeContent={4} color='secondary'>
-                                <NotificationsNoneIcon />
+                                <NotificationsNoneIcon fontSize="small"/>
                             </Badge>
                         </IconButton>
                         <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
                             <Badge badgeContent={4} color='secondary'>
-                                <NotificationsNoneIcon />
+                                <NotificationsNoneIcon fontSize="small"/>
                             </Badge>
                         </IconButton>
                         <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
                             <Badge badgeContent={4} color='secondary'>
-                                <NotificationsNoneIcon />
+                                <NotificationsNoneIcon fontSize="small"/>
                             </Badge>
                         </IconButton>
                     </Grid>
